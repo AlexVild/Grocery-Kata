0d84392151f19a1cfe5615aa934e3ce7240bb1da
AlexVild <ajv2324@gmail.com>
2019-08-29 19:05:39 -0400

    AV - Add functions for special menu; inventory menu

--- src/test/groovy/avild/grocerykata/MenuSpec.groovy
+++ src/test/groovy/avild/grocerykata/MenuSpec.groovy
@@ -4,16 +4,68 @@ import spock.lang.Specification
 
 class MenuSpec extends Specification {
 
-    def "displays a menu"() {
+    def "displays the main menu"() {
         given:
-        Menu menu = new Menu()
         def buffer = new ByteArrayOutputStream()
         System.out = new PrintStream(buffer)
 
         when:
-        menu.displayMenu()
+        Menu.displayMainMenu()
 
         then:
-        buffer.toString().contains("hey")
+        buffer.toString().contains(
+                '''
+                Main Menu:/n
+                [1] Modify inventory\n
+                [2] Check customer out\n
+                [3] Quit\n
+                \n
+                Enter [num]: 
+                ''')
+    }
+
+    def "displays the inventory menu"() {
+        given:
+        def buffer = new ByteArrayOutputStream()
+        System.out = new PrintStream(buffer)
+
+        when:
+        Menu.displayInventoryMenu()
+
+        then:
+        buffer.toString().contains(
+                '''
+                Main Menu > Inventory Menu:/n
+                [1] View Inventory\n
+                [2] Add Item to Inventory\n
+                [3] Update Item Price\n
+                [4] Remove Item from Inventory\n
+                [5] Manage Specials\n
+                [6] Main Menu\n
+                \n
+                Enter [num]: 
+                ''')
+    }
+
+    def "displays the special management menu"() {
+        given:
+        def buffer = new ByteArrayOutputStream()
+        System.out = new PrintStream(buffer)
+
+        when:
+        Menu.displaySpecialMenu()
+
+        then:
+        buffer.toString().contains(
+                '''
+                Main Menu > Inventory Menu > Special Management:/n
+                [1] Add Amount Special (n for m at $x)\n
+                [2] Add Percent Special (buy n get m at x%)\n
+                [3] Add Equal or Lesser Value Special (buy m pounds, get equal or lesser for x%)\n
+                [4] Remove Special\n
+                [5] Inventory Management\n
+                \n
+                Enter [num]: 
+                ''')
     }
 }
